21043bbcae5fabd7682788872073753446dc2754
fabianaperez <96074726+fabianaperez@users.noreply.github.com>
2022-01-03 11:44:31 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,17 +52,17 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("tap-percussive.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("alwaysdestroythethingsilove2-0.ogg");
+export var ambientSound = new soundFile("pongbackground_final.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
 
-export var adventureMusic = new soundFile("alwaysdestroythethingsilove2-0.ogg");
+export var adventureMusic = new soundFile("pongbackground_final.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
 
-export var villageMusic = new soundFile("alwaysdestroythethingsilove2-0.ogg");
+export var villageMusic = new soundFile("pongbackground_final.mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
 villageMusic.player.volume.value = -16;
