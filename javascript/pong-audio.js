963ad78d4308a88b9da50a900ad8ebe1f9d1220b
fabianaperez <96074726+fabianaperez@users.noreply.github.com>
2022-01-13 19:03:30 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,17 +52,17 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("wallsound.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("pongbackground_final_2.mp3");
+export var ambientSound = new soundFile("snailpongbackground_final_2.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = 4; //turn down volume
 
-export var adventureMusic = new soundFile("pongbackground_final_2.mp3");
+export var adventureMusic = new soundFile("snailpongbackground_final_2.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
 
-export var villageMusic = new soundFile("pongbackground_final_2.mp3");
+export var villageMusic = new soundFile("snailpongbackground_final_2.mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
 villageMusic.player.volume.value = -16;
